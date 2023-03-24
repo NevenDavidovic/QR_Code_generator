47f87e2508397b904c09df69fda9bd92965d1103
Neven Davidović <57957655+NevenDavidovic@users.noreply.github.com>
2023-03-25 00:29:55 +0100

    Add files via upload

---
+++ bar.py
@@ -0,0 +1,54 @@
+import pyqrcode
+import png
+choice=True
+while choice:
+    print("1 Payment Data")
+    print("2 From a chunk of text to barcode")
+    print("0 Exit")
+    
+    choice=input("Odaberi broj: ")  
+    
+    if choice=='1':
+        # Ask for the person's name and last name, IBAN, and payment amount
+        name = input("Enter your name: ")
+        last_name = input("Enter your last name: ")
+        iban = input("Enter your IBAN: ")
+        amount = input("Enter payment amount: ")
+
+        # Construct the payment data string
+        payment_data = f"Name: {name}\nLast Name: {last_name}\nIBAN: {iban}\nAmount: {amount}"
+
+        # Generate the QR code
+        qr_code = pyqrcode.create(payment_data, error='H')
+
+        # Save the QR code as a PNG image file
+        filename = "nfc_payment.png"
+        qr_code.png(filename, scale=10, module_color=[0, 0, 0, 255], background=[0xff, 0xff, 0xff, 0xff])
+        
+    
+    elif choice=='2':
+        # Ask for the text to be encoded in QR code
+        text = input("Enter the text to be encoded in QR code: ")
+
+        # Generate the QR code
+        qr_code = pyqrcode.create(text, error='H')
+
+        # Save the QR code as a PNG image file
+        filename = "qr_code.png"
+        qr_code.png(filename, scale=10, module_color=[0, 0, 0, 255], background=[0xff, 0xff, 0xff, 0xff])
+        
+    elif choice=='0':
+        # Exit the program
+        choice=False
+        
+    else:
+        # Invalid input, ask for input again
+        print("Invalid input. Please enter a valid choice.\n")     
+        
+   
+
+
+
+
+
+
